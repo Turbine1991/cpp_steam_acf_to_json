1aff99c61e3d3187898f96ef8a8c34aec08892c1
Unknown <turbine1991@gmail.com>
2018-05-08 01:05:54 +1200

    Initial content commit.

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-cpp_steam_acf_to_json
+# Steam ACF to JSON
+
+This script is a quick solution to enable ACF's to be queried as if it were JSON. It has application in trivialising version checking, obtaining branches available and various other tools.
+
+Linux Support: x86, x86_x64, arm, armhf
+
+Windows Support: x86, x86_x64
+
+# Examples
+### File examples - Get buildid
+> acf_to_json test_appmanifest_file.acf | jq_x86 -r .AppState.buildid
+
+```1110931```
+
+> cat test_appmanifest_steamcmd.acf | acf_to_json | jq_x86 -r ".[\"4940\"].depots.branches.public.buildid"
+
+```2660979```
+
+### Steamcmd example - Get buildid
+> ./steamcmd/steamcmd.sh +login anonymous +app_info_update 1 +app_info_print "740" +app_info_print "740" +quit | acf_to_json | jq_x86 -r ".[\"740\"].depots.branches.public.buildid"
+
+```2735003```
+
+### Steamcmd example - Get all branches [branche buildid timestamp_updated]
+> ./steamcmd/steamcmd.sh +login anonymous +app_info_update 1 +app_info_print "740" +app_info_print "740" +quit | acf_to_json | jq_x86 -r ".[\"740\"].depots.branches | to_entries[] | [.key, .value.buildid, .value.timeupdated, .value.pwdrequired] | @tsv"
+
+```1.36.3.1  2646665 1524265990
+
+1.36.3.2  2708674 1525113411
+
+1.36.3.3  2735003 1525131007
+
+dprs      2479775 1517267346      1
+
+public    2735003 1525130931
+```
+
+# Linux Setup
+### Clone repository in terminal (please install git beforehand)
+> git clone https://github.com/Turbine1991/cpp_steam_acf_to_json.git && cd cpp_steam_acf_to_json
+
+### Install binary globally [x86, x86_x64, arm, armhf]
+> su && cp acf_to_json_x86 /usr/local/bin/acf_to_json && exit
+
+### Download steamcmd locally for example
+> wget "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" -P steamcmd && tar -zxf steamcmd/steamcmd_linux.tar.gz -C steamcmd
+
+# Windows Setup
+Install GIT https://git-scm.com/downloads
+
+> git clone https://github.com/Turbine1991/cpp_steam_acf_to_json.git && cd cpp_steam_acf_to_json
+
+### Install binary globally [x86, x86_x64]
+Open CMD as Administrator and `cd` to cloned path
+
+> mkdir C:\bin
+
+> setx PATH "%path%;C:\bin"
+
+> copy /y acf_to_json_x86.exe C:\bin\acf_to_json.exe
+
---
+++ src/main.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "json.hpp"
+
+using namespace std;
+class Acf {
+  string data;
+
+public:
+  Acf(string data): data(data) {
+    cropData();
+    toJsonRaw();
+    toJson();
+  }
+
+  void cropData() {
+    std::size_t found = -1;
+    string delims = "{}";
+    int opened = 0;
+    int indexOpen = -1, indexClose = -1;
+    while((found = data.find_first_of(delims, found + 1)) != std::string::npos && indexClose == -1)
+    {
+      char c = data.at(found);
+
+      if (c == '{') {
+        opened++;
+
+        if (indexOpen == -1)
+          indexOpen = found;
+      } else if (c == '}') {
+        opened--;
+
+        if (opened == 0) {
+          indexClose = found + 1;
+        }
+      }
+    }
+
+    // Get key for table
+    found = data.find_last_of("\"", indexOpen);
+    found = data.find_last_of("\"", found - 1);
+    indexOpen = found;
+
+    data = data.substr(indexOpen, indexClose - indexOpen);
+  }
+
+  void toJsonRaw() {
+    int indexQuote = 0;
+    int index[4];
+    int insertCommaNext = -1;
+    string delims = "\"{}";
+    std::size_t found = data.find_first_of(delims);
+    while(found != std::string::npos)
+    {
+      int inc = 1;
+      char c = data.at(found);
+
+      if (c != '"') {
+        if (c == '}')
+          insertCommaNext = found + 1;
+        else if (c == '{') {
+          data.insert(index[1] + 1, ":");
+          inc++;
+        }
+
+        indexQuote = 0;
+      } else {
+        if (insertCommaNext != -1) {
+          data.insert(insertCommaNext, ",");
+          inc++;
+          insertCommaNext = -1;
+        }
+
+        if (indexQuote == 2) { // Join 'key: value'
+          data.replace(index[1] + 1, 1, ":");
+        } else if (indexQuote == 4) {
+          indexQuote = 0;
+          data.insert(index[3] + 1, ",");
+          inc++;
+        }
+
+        index[indexQuote] = found;
+
+        indexQuote++;
+      }
+
+      found = data.find_first_of(delims, found + inc);
+    }
+
+    data = "{" + data + "}";
+  }
+
+  void toJson() {
+    picojson::value json;
+    std::string err = picojson::parse(json, data);
+    if (!err.empty())
+      std::cerr << err << std::endl;
+
+    data = json.serialize();
+  }
+
+  void print() {
+    cout << data;
+  }
+
+  string get() {
+    return data;
+  }
+};
+
+int main(int argc, char* argv[])
+{
+  if (argc >= 2) { // Filename
+    std::string filename(argv[1]);
+    std::ifstream file;
+    std::ios_base::iostate exceptionMask = file.exceptions() | std::ios::failbit;
+    file.exceptions(exceptionMask);
+
+    try {
+      file.open(filename);
+      std::string data((std::istreambuf_iterator<char>(file)), (std::istreambuf_iterator<char>()));
+
+      Acf acf(data);
+      acf.print();
+    }
+    catch (std::ios_base::failure& e) {
+      std::cerr << e.what() << '\n';
+    }
+  } else { // stdin
+    std::string data = "";
+    for (std::string line; std::getline(std::cin, line);)
+      data += line;
+
+    Acf acf(data);
+    acf.print();
+  }
+
+  return 0;
+}
